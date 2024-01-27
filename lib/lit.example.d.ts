83d322488b36581af0dd44439a29d76667c00b4e
Flippie.Coetser <flippie.coetser@gmail.com>
2024-01-27 05:13:48 -0800

    Build

--- lib/lit.example.d.ts
+++ lib/lit.example.d.ts
@@ -1,6 +1,5 @@
 import { LitElement } from "lit";
 export declare class MyElement extends LitElement {
-    constructor();
     accessor myProperty: string;
     render: () => import("lit-html").TemplateResult<1>;
     updateProperty: () => void;
