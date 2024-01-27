2b2948b4e8fbc9685e9fd5911139495b3c592307
Flippie.Coetser <flippie.coetser@gmail.com>
2024-01-27 04:56:31 -0800

    WIP Unit Test

--- test/lit.example.test.ts
+++ test/lit.example.test.ts
@@ -1,8 +1,16 @@
 import { MyElement } from "../src/lit.example.js";
 
-describe("D3.Example", () => {
-  it("should run", () => {
+describe("lit.Example", () => {
+  beforeAll(() => {
     customElements.define("my-element", MyElement);
+  });
+  it("component should be attached to DOM", () => {
     const element = document.createElement("my-element");
+    expect(element).toBeInstanceOf(MyElement);
+  });
+  it("custom event should fire on button click", () => {
+    const element = document.createElement("my-element");
+    const button = element.querySelector("button");
+    button?.click();
   });
 });
