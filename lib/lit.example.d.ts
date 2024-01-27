0128384c0d79d28899f10d54aca5f4106c73d24b
Flippie.Coetser <flippie.coetser@gmail.com>
2024-01-27 04:53:03 -0800

    Add Custom Event

--- lib/lit.example.d.ts
+++ lib/lit.example.d.ts
@@ -1,5 +1,7 @@
 import { LitElement } from "lit";
 export declare class MyElement extends LitElement {
+    constructor();
     accessor myProperty: string;
     render: () => import("lit-html").TemplateResult<1>;
+    updateProperty: () => void;
 }
--- src/lit.example.ts
+++ src/lit.example.ts
@@ -2,8 +2,30 @@ import { LitElement, html } from "lit";
 import { property } from "lit/decorators.js";
 
 export class MyElement extends LitElement {
-  @property()
+  constructor() {
+    super();
+    // for testing a custom event on component
+    this.addEventListener("my-event", (e) => {
+      console.log("my-event");
+    });
+  }
+  // attribute is used to define a custom attribute name associated with this property
+  // reflect is used to establish a two-way data binding between the property and the attribute
+  @property({ attribute: "attribute", type: String, reflect: true })
   accessor myProperty = "property value";
 
-  render = () => html`<p>${this.myProperty}</p>`;
+  render = () => html`
+    <p>${this.myProperty}</p>
+    <button @click=${this.updateProperty}>Update</button>
+  `;
+
+  updateProperty = () => {
+    this.myProperty = "new value";
+    const event = new CustomEvent("my-event", {
+      bubbles: true,
+      composed: true,
+      detail: { message: "Hello World!" },
+    });
+    this.dispatchEvent(event);
+  };
 }
